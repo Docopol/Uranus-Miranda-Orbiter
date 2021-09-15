806299440660529c8f16c2786d0033229687a2bc
GardenerGeralt <89786138+GardenerGeralt@users.noreply.github.com>
2021-09-15 21:23:21 +0200

    Add files via upload

---
+++ input_parameters.py
@@ -0,0 +1,20 @@
+# # # natural constants # # #
+g_0 = 9.81
+
+# # # required parameters # # #
+m = 372.8                   # [kg] max structure mass
+M = 8884.84                 # [kg] total s/c mass
+g_axial = 7.5 * g_0         # [m/s^2] max axial load
+g_lateral = 2.5 * g_0       # [m/s^2] max lateral load
+f_n = 1250.                 # [Hz] min natural frequency
+V = 83.82                   # [m^2] max s/c volume
+d = 3.6                     # [m] diameter of s/c
+
+# # # material parameters # # #
+# material: Aluminum 1111
+rho = 2780          # [kg/m^3] density
+E = 72              # [GPa] elastic modulus
+G = 27              # [GPa] shear modulus
+sigma_y = 345       # [MPa] yield stress
+sigma_u = 483       # [MPa] ultimate stress
+
---
+++ main.py
@@ -0,0 +1,73 @@
+import input_parameters as i
+import math
+
+
+def dimensions(d, V, th):   # inner diameter, maximal volume
+    r = d / 2                       # [m] inner radius
+    a = math.pi * (r ** 2)          # [m^2] inner area
+    h = V / a                       # [m] height
+    R = r + th                       # [m] outer radius
+    A = math.pi * (R ** 2)          # [m^2] outer area
+    ca = A - a                      # [m^2] cross sectional area
+    I_a = math.pi * th * (R ** 3)    # [m^4] second moment of area
+    return {
+        "Height": h,
+        "Outer Radius": R,
+        "Cross Sectional Area": ca,
+        "Outer Area": A,
+        "Second Moment of Area": I_a
+    }
+
+
+def stresses(M, g_a, g_l, dims):    # s/c mass, axial load, lateral load, dimensions
+    A = dims["Cross Sectional Area"]
+    h = dims["Height"]
+    r = dims["Outer Radius"]
+    I_a = dims["Second Moment of Area"]
+    P_a = M * g_a                               # [N] axial load
+    V = M * g_l                                 # [N] lateral load
+    sigma_a = (P_a / A) * 1e-6                  # [MPa] axial stress
+    sigma_l = (V * (h / 2) * r / I_a) * 1e-6    # [MPa] bending stress
+    sigma_n = sigma_a + sigma_l                 # [MPa] maximal normal stress
+    tau_av = (V / A) * 1e-6                     # [MPa] average shear stress
+    return {
+        "Max Normal Stress": round(sigma_n, 1),
+        "Average Shear Stress": round(tau_av, 1)
+    }
+
+
+def strains(strss, E, G):       # stresses, elastic modulus, shear modulus
+    sigma_n = strss["Max Normal Stress"]
+    tau_av = strss["Average Shear Stress"]
+    epsilon = sigma_n / E       # [mm] normal strain
+    gamma = tau_av / G          # [mm] shear strain
+    return {
+        "Normal Strain": round(epsilon, 3),
+        "Shear Strain": round(gamma, 3)
+    }
+
+
+def mass(dims, rho, th):
+    h = dims["Height"]
+    R = dims["Outer Radius"]
+    V = (math.pi * (R ** 2) + math.pi * 2 * R * h) * th      # [m^3] total volume of structural elements
+    m = V * rho     # [kg] total structure mass
+    return round(m, 1)
+
+
+t = 0.0001      # [m] wall thickness
+dt = 0.0001     # [m] change in thickness per iteration
+
+while True:
+    Dimensions = dimensions(i.d, i.V, t)    # compute dimensions
+
+    Stresses = stresses(i.M, i.g_axial, i.g_lateral, Dimensions)    # compute stresses
+    if (Stresses["Max Normal Stress"] or Stresses["Average Shear Stress"]) > i.sigma_y:
+        t += dt     # if stresses too high increase thickness and iterate again
+        continue
+
+    Strains = strains(Stresses, i.E, i.G)       # compute strains
+    m = mass(Dimensions, i.rho, t)     # compute structure mass
+    break
+
+print(m, t)
