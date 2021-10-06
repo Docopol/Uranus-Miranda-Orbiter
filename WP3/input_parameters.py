35e1f0621507bc8323a373ab3840c0acd67e9462
swelters <s.j.welters@student.tudelft.nl>
2021-10-06 16:44:14 +0200

    Update input_parameters.py

--- WP3/input_parameters.py
+++ WP3/input_parameters.py
@@ -1,5 +1,5 @@
 # # # natural constants # # #
-
+import numpy as np
 g_param = 5.793939 * 1e6    # [km^3/s^2] Gravitational Parameter
 r_uranus = 25362            # [km] Radius of Uranus
 rho = 8.417 * 1e-13         # [km/m^3] Density at orbit height
@@ -10,7 +10,7 @@ P_s = 2.468 * 1e-8          # [N/m^2] Solar pressure around Uranus
 r_cylinder = 1.8            # [m] radius of circular cylinder
 len_cylinder = 8            # [m] length of cylinder
 r_orbit = 4343              # [km] Radius of circular orbit
-v_orbit = 14011             # [m/s] Orbital velocity
+v_orbit = np.sqrt(g_param/(r_orbit+r_uranus))             # [m/s] Orbital velocity
 
 # # # masses # # #
 
@@ -27,7 +27,7 @@ areas = {"Area1": [0, 0, 0, 1],                     # [m^2] x, y, z, area
 
 n_sq = (g_param / (r_orbit + r_uranus) ** 3)        # [rad/s]parameter for Gravity torque
 
-C_d = 2.7                                           # [-] drag coefficient for Aerodynamic torque
+C_d = 2.6                                           # [-] drag coefficient for Aerodynamic torque
 S = [0, 0, 0]                                       # [m^2] surface area for Aerodynamic torque
 
 rho_r = 0.84                                        # [-] reflectivity of sail for solar radiation torque
