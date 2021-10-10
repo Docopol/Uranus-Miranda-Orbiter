59e9064a19693cc42ccd99d1eb4da97a216a136b
Kamalesh666 <63967068+Kamalesh666@users.noreply.github.com>
2021-10-10 21:07:26 +0200

    Updated the program

--- WP3/input_parameters.py
+++ WP3/input_parameters.py
@@ -11,16 +11,22 @@ P_s = 2.468 * 1e-8  # [N/m^2] Solar pressure around Uranus
 m_cylinder = 157.8  # [kg] mass of structure
 r_cylinder = 1.8  # [m] radius of circular cylinder
 h_cylinder = 8.235  # [m] length of cylinder
-r_orbit = 4343  # [km] Radius of circular orbit
+r_orbit = 4343  # [km] Altitude of circular orbit
+a_orbit = 2 * (r_uranus + r_orbit)  # Semi Major Axis of orbit
 v_orbit = np.sqrt(g_param / (r_orbit + r_uranus)) * 1e3  # [m/s] Orbital velocity
+t_orbit = 2 * np.pi * np.sqrt(np.power(a_orbit, 3, dtype="int64") / g_param)  # orbital time period
+t_mission = 4*365.25*24*3600
 a = 1.378  # [m] side length of octagon
 s_proj = a * (1 + np.sqrt(2))  # projected side length
-I_x = (0.345 * m_cylinder * r_cylinder**4) / (a * r_cylinder)  # [kg*m^2] Moment of Inertia along x axis
+I = np.array([(0.345 * m_cylinder * r_cylinder ** 4) / (a * r_cylinder), 0, 0])  # [kg*m^2] Moment of Inertia along x axis
 
 # # # masses # # #
 
-masses = {"Mass1": [0, 0, 0, 1],  # [kg] x, y, z, mass
-          "Mass2": [0, 1, 0, 1]}
+masses_init = {"Mass of System": [0, 0, 0, 1517.24],  # [kg] x, y, z, mass
+               "Mass of Fuel": [0.702, 0, 0, 180.73 * 1.2]}
+
+masses_fin = {"Mass of System": [0, 0, 0, 1517.24],  # [kg] x, y, z, mass
+              "Mass of Fuel": [0.702, 0, 0, (180.73 * (1 - 1 / 1.02) * 1.2)]}  # removing 2% residual
 
 # # # areas # # #
 
@@ -32,7 +38,7 @@ areas = {"Area1": [0, 0, 0, 1],  # [m^2] x, y, z, area
 n_sq = (g_param / (r_orbit + r_uranus) ** 3)  # [rad/s]parameter for Gravity torque
 
 C_d = 2.6  # [-] drag coefficient for Aerodynamic torque
-S = [9.17, s_proj*h_cylinder, s_proj*h_cylinder]  # [m^2] surface area for Aerodynamic torque
+S = [9.17, s_proj * h_cylinder, s_proj * h_cylinder]  # [m^2] surface area for Aerodynamic torque
 
 rho_r = 0.84  # [-] reflectivity of sail for solar radiation torque
 S_eff = 28.8  # [m^2] effective area of of the S/C for solar radiation torque
--- WP3/main.py
+++ WP3/main.py
@@ -1,5 +1,6 @@
 import input_parameters as i
 import numpy as np
+from scipy.integrate import quad
 
 
 def COM(masses):
@@ -7,9 +8,7 @@ def COM(masses):
     for k, v in masses.items():
         mass_array = np.vstack((mass_array, v))
     c_mass = np.average(mass_array[:, :3], axis=0, weights=mass_array[:, 3])
-    return {
-        "Centre of Mass": c_mass
-    }
+    return c_mass
 
 
 def COP(areas):
@@ -35,18 +34,16 @@ def gravity_torque(I_x, I_y, I_z, phi, theta):
     }
 
 
-def aerodynamic_torque(r_m, S):
-    F_a = 0.5 * rho * v**2 * S * C_d
+def aerodynamic_torque(r_m, S, v):
+    F_a = 0.5 * i.rho * i.C_d * np.square(v) * S
     torque = np.cross(r_m, F_a)
-    return {
-        "Aerodynamic torque": torque
-    }
+    return torque
 
 
 def solar_torque(r_p, S):
     F_s = (1 + rho) * P_s * S
     torque = np.cross(r_p, F_s)
-    return{
+    return {
         "Solar radiation torque": torque
     }
 
@@ -58,5 +55,37 @@ def magnetic_torque(M, B):
     }
 
 
-print(COM(i.masses))
-print(COP(i.areas))
+def sin(x):
+    return np.sin(x)
+
+
+def ang_impulse(S, v, angle):
+    av_sin = quad(sin, 0, angle)
+    F_a = 0.5 * i.rho * i.C_d * np.square(v) * (S[0] + S[1]) * av_sin
+
+
+def calculations():
+    cm_array_init = COM(i.masses_init)
+    cm_array_fin = COM(i.masses_fin)
+    cm_array_avg = (cm_array_init + cm_array_fin)/2
+
+    aero_torque_mission = aerodynamic_torque(cm_array_avg, i.S, v_mission)
+    aero_torque_sending = aerodynamic_torque(cm_array_avg, i.S, v_sending)
+    impulse_mission = aero_torque_mission * i.t_mission * 3 / 4
+    impulse_sending = aero_torque_sending * i.t_mission * 1 / 4
+    impulse_mission_orbit = aero_torque_mission * i.t_orbit * 3 / 4
+    impulse_sending_orbit = aero_torque_sending * i.t_orbit * 1 / 4
+
+    ang_impulse_sending = i.I[1] * np.pi/2 / i.t_orbit/4
+
+    return {
+        "Impulse total: ": impulse_mission + impulse_sending,
+        "Impulse 1 orbit Total: ": impulse_mission_orbit + impulse_sending_orbit + ang_impulse_sending
+    }
+
+
+v_mission = np.array([i.v_orbit, 0, 0])
+v_sending = np.array([0, i.v_orbit, 0])
+
+print(COM(i.masses_init))
+print(calculations())
