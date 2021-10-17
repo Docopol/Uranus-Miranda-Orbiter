68acf5cd4091de3d97b6d512c7a6da6595cbd159
Kamalesh666 <63967068+Kamalesh666@users.noreply.github.com>
2021-10-18 00:26:21 +0200

    Added aero torque in x, y and z components

--- WP3/graphs.py
+++ WP3/graphs.py
@@ -12,13 +12,21 @@ g_torque = []
 m_torque = []
 sum_torques = []
 a_x = []
+a_y = []
+a_z = []
 
 for i in t:
     if 0 <= i <= 1/4:
         ae_torque = np.sqrt(np.square(main.torque_ae_x_var(i * To)) + np.square(main.torque_ae_y_var(i * To)) + np.square(main.torque_ae_z_var(i * To)))
         a_torque.append(ae_torque)
+
         x = main.torque_ae_x_var(i * To)
+        y = main.torque_ae_y_var(i * To)
+        z = main.torque_ae_z_var(i * To)
+
         a_x.append(x)
+        a_y.append(y)
+        a_z.append(z)
 
         sol_torque = norm(main.solar_torque(main.cm_array_avg))
         s_torque.append(sol_torque)
@@ -31,8 +39,14 @@ for i in t:
     if 1/4 < i <= (1/2 + ip.frac_orbit_before_90deg_sun):
         ae_torque = norm(main.aerodynamic_torque(main.cm_array_avg, main.v_mission))
         a_torque.append(ae_torque)
+
         x = main.aerodynamic_torque(main.cm_array_avg, main.v_mission)[0]
+        y = main.aerodynamic_torque(main.cm_array_avg, main.v_mission)[1]
+        z = main.aerodynamic_torque(main.cm_array_avg, main.v_mission)[2]
+
         a_x.append(x)
+        a_y.append(y)
+        a_z.append(z)
 
         sol_torque = np.sqrt(np.square(main.torque_s_x_var(i * To)) + np.square(main.torque_s_y_var(i * To)) + np.square(main.torque_s_z_var(i * To)))
         s_torque.append(sol_torque)
@@ -45,8 +59,14 @@ for i in t:
     if (1/2 + ip.frac_orbit_before_90deg_sun) < i <= (1-ip.frac_orbit_before_90deg_sun):
         ae_torque = norm(main.aerodynamic_torque(main.cm_array_avg, main.v_mission))
         a_torque.append(ae_torque)
+
         x = main.aerodynamic_torque(main.cm_array_avg, main.v_mission)[0]
+        y = main.aerodynamic_torque(main.cm_array_avg, main.v_mission)[1]
+        z = main.aerodynamic_torque(main.cm_array_avg, main.v_mission)[2]
+
         a_x.append(x)
+        a_y.append(y)
+        a_z.append(z)
 
         sol_torque = 0
         s_torque.append(sol_torque)
@@ -59,8 +79,14 @@ for i in t:
     if (1-ip.frac_orbit_before_90deg_sun) < i <= 1:
         ae_torque = np.sqrt(np.square(main.torque_ae_x_var(i * To)) + np.square(main.torque_ae_y_var(i * To)) + np.square(main.torque_ae_z_var(i * To)))
         a_torque.append(ae_torque)
+
         x = main.torque_ae_x_var(i * To)
+        y = main.torque_ae_y_var(i * To)
+        z = main.torque_ae_z_var(i * To)
+
         a_x.append(x)
+        a_y.append(y)
+        a_z.append(z)
 
         sol_torque = norm(main.solar_torque(main.cm_array_avg))
         s_torque.append(sol_torque)
@@ -78,7 +104,10 @@ def aero_graph():
     plt.title("Aerodynamic torque")
     plt.xlabel("t/t_orbit")
     plt.ylabel("Torque [Nm]")
-    plt.plot(t, a_torque)
+    plt.plot(t, a_torque, 'k', label = 'total aerodynamic torque')
+    plt.plot(t, a_x, 'r', label = 'x-axis')
+    plt.plot(t, a_y, 'b', label = 'y-axis')
+    plt.plot(t, a_z, 'g', label = 'z-axis')
     plt.savefig("aero_graph.png")
     plt.show()
 
@@ -118,15 +147,9 @@ def tot_graph():
     plt.savefig("total_graph.png")
     plt.show()
 
-plt.subplot(3, 2, 6)
-plt.plot(t, a_x)
-plt.tight_layout()
-plt.show()
-
 
 aero_graph()
 solar_graph()
 grav_graph()
 mag_graph()
 tot_graph()
-
