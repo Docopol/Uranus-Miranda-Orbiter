7f64882143d6f51c2f43911980fdf50b5a81021b
swelters <s.j.welters@student.tudelft.nl>
2021-10-18 01:08:20 +0200

    Graphs

--- WP3/graphs.py
+++ WP3/graphs.py
@@ -41,12 +41,28 @@ for i in t:
 
         sol_torque = norm(main.solar_torque(main.cm_array_avg))
         s_torque.append(sol_torque)
+        s_x = main.solar_torque(main.cm_array_avg)[0]
+        s_y = main.solar_torque(main.cm_array_avg)[1]
+        s_z = main.solar_torque(main.cm_array_avg)[2]
+        s_T_x.append(s_x)
+        s_T_y.append(s_y)
+        s_T_z.append(s_z)
 
         grav_torque = np.sqrt(np.square(main.torque_grav_x_var(i * To)) + np.square(main.torque_grav_y_var(i * To)) + np.square(main.torque_grav_z_var(i * To)))
         g_torque.append(grav_torque)
+        g_x = main.torque_grav_x_var(i * To)
+        g_y = main.torque_grav_y_var(i * To)
+        g_z = main.torque_grav_z_var(i * To)
+        grav_T_x.append(g_x)
+        grav_T_y.append(g_y)
+        grav_T_z.append(g_z)
 
         mag_torque = main.magnetic_torque(ip.D, ip.B_uranus)[1]
         m_torque.append(mag_torque)
+        mag_T_x.append(mag_torque / 3)
+        mag_T_y.append(mag_torque / 3)
+        mag_T_z.append(mag_torque / 3)
+
     if 1/4 < i <= (1/2 + ip.frac_orbit_before_90deg_sun):
         ae_torque = norm(main.aerodynamic_torque(main.cm_array_avg, main.v_mission))
         a_torque.append(ae_torque)
@@ -59,12 +75,28 @@ for i in t:
 
         sol_torque = np.sqrt(np.square(main.torque_s_x_var(i * To)) + np.square(main.torque_s_y_var(i * To)) + np.square(main.torque_s_z_var(i * To)))
         s_torque.append(sol_torque)
+        s_x = main.torque_s_x_var(i * To)
+        s_y = main.torque_s_y_var(i * To)
+        s_z = main.torque_s_z_var(i * To)
+        s_T_x.append(s_x)
+        s_T_y.append(s_y)
+        s_T_z.append(s_z)
 
         grav_torque = norm(main.gravity_torque(ip.I_xx, ip.I_yy, ip.I_zz, ip.phi_misalignment, ip.theta_misalignment))
         g_torque.append(grav_torque)
+        g_x = main.gravity_torque(ip.I_xx, ip.I_yy, ip.I_zz, ip.phi_misalignment, ip.theta_misalignment)[0]
+        g_y = main.gravity_torque(ip.I_xx, ip.I_yy, ip.I_zz, ip.phi_misalignment, ip.theta_misalignment)[1]
+        g_z = main.gravity_torque(ip.I_xx, ip.I_yy, ip.I_zz, ip.phi_misalignment, ip.theta_misalignment)[2]
+        grav_T_x.append(g_x)
+        grav_T_y.append(g_y)
+        grav_T_z.append(g_z)
 
         mag_torque = main.magnetic_torque(ip.D, ip.B_uranus)[1]
         m_torque.append(mag_torque)
+        mag_T_x.append(mag_torque / 3)
+        mag_T_y.append(mag_torque / 3)
+        mag_T_z.append(mag_torque / 3)
+
     if (1/2 + ip.frac_orbit_before_90deg_sun) < i <= (1-ip.frac_orbit_before_90deg_sun):
         ae_torque = norm(main.aerodynamic_torque(main.cm_array_avg, main.v_mission))
         a_torque.append(ae_torque)
@@ -77,12 +109,28 @@ for i in t:
 
         sol_torque = 0
         s_torque.append(sol_torque)
+        s_x = 0
+        s_y = 0
+        s_z = 0
+        s_T_x.append(s_x)
+        s_T_y.append(s_y)
+        s_T_z.append(s_z)
 
         grav_torque = norm(main.gravity_torque(ip.I_xx, ip.I_yy, ip.I_zz, ip.phi_misalignment, ip.theta_misalignment))
         g_torque.append(grav_torque)
+        g_x = main.gravity_torque(ip.I_xx, ip.I_yy, ip.I_zz, ip.phi_misalignment, ip.theta_misalignment)[0]
+        g_y = main.gravity_torque(ip.I_xx, ip.I_yy, ip.I_zz, ip.phi_misalignment, ip.theta_misalignment)[1]
+        g_z = main.gravity_torque(ip.I_xx, ip.I_yy, ip.I_zz, ip.phi_misalignment, ip.theta_misalignment)[2]
+        grav_T_x.append(g_x)
+        grav_T_y.append(g_y)
+        grav_T_z.append(g_z)
 
         mag_torque = main.magnetic_torque(ip.D, ip.B_uranus)[1]
         m_torque.append(mag_torque)
+        mag_T_x.append(mag_torque / 3)
+        mag_T_y.append(mag_torque / 3)
+        mag_T_z.append(mag_torque / 3)
+
     if (1-ip.frac_orbit_before_90deg_sun) < i <= 1:
         ae_torque = np.sqrt(np.square(main.torque_ae_x_var(i * To)) + np.square(main.torque_ae_y_var(i * To)) + np.square(main.torque_ae_z_var(i * To)))
         a_torque.append(ae_torque)
@@ -95,12 +143,27 @@ for i in t:
 
         sol_torque = norm(main.solar_torque(main.cm_array_avg))
         s_torque.append(sol_torque)
+        s_x = main.solar_torque(main.cm_array_avg)[0]
+        s_y = main.solar_torque(main.cm_array_avg)[1]
+        s_z = main.solar_torque(main.cm_array_avg)[2]
+        s_T_x.append(s_x)
+        s_T_y.append(s_y)
+        s_T_z.append(s_z)
 
         grav_torque = np.sqrt(np.square(main.torque_grav_x_var(i * To)) + np.square(main.torque_grav_y_var(i * To)) + np.square(main.torque_grav_z_var(i * To)))
         g_torque.append(grav_torque)
+        g_x = main.torque_grav_x_var(i * To)
+        g_y = main.torque_grav_y_var(i * To)
+        g_z = main.torque_grav_z_var(i * To)
+        grav_T_x.append(g_x)
+        grav_T_y.append(g_y)
+        grav_T_z.append(g_z)
 
         mag_torque = main.magnetic_torque(ip.D, ip.B_uranus)[1]
         m_torque.append(mag_torque)
+        mag_T_x.append(mag_torque / 3)
+        mag_T_y.append(mag_torque / 3)
+        mag_T_z.append(mag_torque / 3)
 
 sum_torques = np.array(a_torque) + np.array(s_torque) + np.array(g_torque) + np.array(m_torque)
 
@@ -109,9 +172,9 @@ def aero_graph():
     plt.title("Aerodynamic torque")
     plt.xlabel("t/t_orbit")
     plt.ylabel("Torque [Nm]")
-    plt.plot(t, ae_T_x, 'r--', label='x-axis')
-    plt.plot(t, ae_T_y, 'b--', label='y-axis')
-    plt.plot(t, ae_T_z, 'g--', label='z-axis')
+    plt.plot(t, ae_T_x, 'r', label='x-axis')
+    plt.plot(t, ae_T_y, 'b', label='y-axis')
+    plt.plot(t, ae_T_z, 'g', label='z-axis')
     plt.legend()
     plt.tight_layout()
     plt.savefig("aero_graph.svg", format="svg")
@@ -122,7 +185,11 @@ def solar_graph():
     plt.title("Solar torque")
     plt.xlabel("t/t_orbit")
     plt.ylabel("Torque [Nm]")
-    plt.plot(t, s_torque)
+    plt.plot(t, s_T_x, 'r', label='x-axis')
+    plt.plot(t, s_T_y, 'b', label='y-axis')
+    plt.plot(t, s_T_z, 'g', label='z-axis')
+    plt.legend()
+    plt.tight_layout()
     plt.savefig("solar_graph.svg", format="svg")
     plt.show()
 
@@ -131,8 +198,12 @@ def grav_graph():
     plt.title("Gravity torque")
     plt.xlabel("t/t_orbit")
     plt.ylabel("Torque [Nm]")
-    plt.plot(t, g_torque)
-    plt.savefig("grav_graph.png")
+    plt.plot(t, grav_T_x, 'r', label='x-axis')
+    plt.plot(t, grav_T_y, 'b', label='y-axis')
+    plt.plot(t, grav_T_z, 'g', label='z-axis')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("grav_graph.svg", format="svg")
     plt.show()
 
 
@@ -141,16 +212,25 @@ def mag_graph():
     plt.xlabel("t/t_orbit")
     plt.ylabel("Torque [Nm]")
     plt.plot(t, m_torque)
-    plt.savefig("mag_graph.png")
+    plt.tight_layout()
+    plt.savefig("mag_graph.svg", format="svg")
     plt.show()
 
+sum_T_x = np.abs(ae_T_x) + np.abs(s_T_x) + np.abs(grav_T_x) + np.abs(mag_T_x)
+sum_T_y = np.abs(ae_T_y) + np.abs(s_T_y) + np.abs(grav_T_y) + np.abs(mag_T_y)
+sum_T_z = np.abs(ae_T_z) + np.abs(s_T_z) + np.abs(grav_T_z) + np.abs(mag_T_z)
 
 def tot_graph():
     plt.title("Total torque")
     plt.xlabel("t/t_orbit")
     plt.ylabel("Torque [Nm]")
-    plt.plot(t, sum_torques)
-    plt.savefig("total_graph.png")
+    plt.plot(t, sum_torques, 'k', label='Magnitude')
+    plt.plot(t, sum_T_x, 'r--', label='x-axis')
+    plt.plot(t, sum_T_y, 'b--', label='y-axis')
+    plt.plot(t, sum_T_z, 'g--', label='z-axis')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("total_graph.svg", format="svg")
     plt.show()
 
 
