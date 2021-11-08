1ce7df265eb3ccf4b5f76f86919bbf53403ea60c
javialonso05 <javialonso05@gmail.com>
2021-11-08 17:16:38 +0100

    Creation of sub-class multi_Lug

--- WP4-5/Classes.py
+++ WP4-5/Classes.py
@@ -1,8 +1,13 @@
-# For simplicity, double lugs will be called Lug
 class Lug:
     def __init__(self, width, lug_thickness, hinge_diameter):
         self.w = width
         self.t = lug_thickness
         self.d = hinge_diameter
 
+
+class multi_Lug(Lug):  # Assumes lug separation will be the same and lugs will be identical
+    def __init__(self, w, lug_t, diameter, separation, number):
+        Lug.__init__(self, w, lug_t, diameter)
+        self.n = number
+        self.h = separation
 
