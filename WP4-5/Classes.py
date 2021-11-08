672d3d59c7e3f03f5118746dd7713a1ac4963856
javialonso05 <javialonso05@gmail.com>
2021-11-08 17:22:48 +0100

    Creation of material class

--- WP4-5/Classes.py
+++ WP4-5/Classes.py
@@ -1,13 +1,19 @@
 class Lug:
-    def __init__(self, width, lug_thickness, hinge_diameter):
+    def __init__(self, width, lug_thickness, hinge_diameter, material):
         self.w = width
         self.t = lug_thickness
         self.d = hinge_diameter
+        self.m = material
 
 
 class multi_Lug(Lug):  # Assumes lug separation will be the same and lugs will be identical
-    def __init__(self, w, lug_t, diameter, separation, number):
-        Lug.__init__(self, w, lug_t, diameter)
+    def __init__(self, width, lug_thickness, hinge_diameter, material, separation, number):
+        Lug.__init__(self, width, lug_thickness, hinge_diameter, material)
         self.n = number
         self.h = separation
 
+
+class Material:
+    def __init__(self, Youngs_Modulus, critical_stress):
+        self.e = Youngs_Modulus
+        self.cr = critical_stress
