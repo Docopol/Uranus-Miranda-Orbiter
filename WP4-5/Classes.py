81bce885689e49fd015b59d7f03f8585d1fafb0b
javialonso05 <javialonso05@gmail.com>
2021-11-08 16:58:05 +0100

    Creation of class Lug

---
+++ WP4-5/Classes.py
@@ -0,0 +1,8 @@
+# For simplicity, double lugs will be called Lug
+class Lug:
+    def __init__(self, width, lug_thickness, hinge_diameter):
+        self.w = width
+        self.t = lug_thickness
+        self.d = hinge_diameter
+
+
