cb3f87e82c6828419f3b1cee75816e14ff96bb05
javialonso05 <javialonso05@gmail.com>
2021-11-10 14:14:11 +0100

    Start of minimum_thickness function

--- WP4-5/Classes.py
+++ WP4-5/Classes.py
@@ -4,7 +4,28 @@ class Lug:
         self.t = lug_thickness
         self.d = hinge_diameter
         self.m = material
-        
+
+
+    def minimum_t(self, load):
+        fx, fy, fz = load
+
+        def t_yield_z():
+            area = (self.w-self.d)  # per unit thickness
+            stress_concentration= 2
+            return stress_concentration * fz / (self.m.get_stress() * area)
+
+        def t_yield_y():
+            pass
+
+        def t_shear():
+            pass
+
+        t1 = t_yield_z()
+        t2 = t_yield_y()
+        t3 = t_shear()
+
+        thickness = sorted([t1, t2, t3])
+        return thickness[2]
 
 
 class multi_Lug:  # Assumes lug separation will be the same and lugs will be identical
