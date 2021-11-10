021209a156d8441bc2f3861544ec0be0c18ee811
javialonso05 <javialonso05@gmail.com>
2021-11-10 15:19:50 +0100

    minimum_t is finished for tensile forces so long as maximum bearing stress is a material property

--- WP4-5/Classes.py
+++ WP4-5/Classes.py
@@ -1,3 +1,5 @@
+import math
+
 class Lug:
     def __init__(self, width, lug_thickness, hinge_diameter, material):
         self.w = width
@@ -9,19 +11,20 @@ class Lug:
     def minimum_t(self, load):
         fx, fy, fz = load
 
-        def t_yield_z():  # Extracted from Bruhn
+        # Failure due to tensile forces - Extracted from Bruhn
+        def t_yield_z():
             area = (self.w-self.d)  # per unit thickness
-            stress_concentration= 1.25
-            return stress_concentration * fz / (self.m.get_stress() * area)
+            return fz / (self.m.get_stress() * area)
 
-        def t_yield_y():
-            pass
+        def t_bearing():
+            return fz / (self.m.get_bear() * self.d)
 
         def t_shear():
-            pass
+            area = 2*math.sqrt((self.w/2)**2 + (self.d/2)**2)  # conservative estimate - per unit thickness
+            return fz / (self.m.get_shear() * area)
 
         t1 = t_yield_z()
-        t2 = t_yield_y()
+        t2 = t_bearing()
         t3 = t_shear()
 
         thickness = sorted([t1, t2, t3])
@@ -42,12 +45,24 @@ class D_lug:
 
 
 class Material:
-    def __init__(self, Youngs_Modulus, critical_stress):
+    def __init__(self, Youngs_Modulus, critical_stress, shear_modulus, maximum_shear, max_bearing_stress):
         self.e = Youngs_Modulus
         self.cr = critical_stress
+        self.g = shear_modulus
+        self.sh = maximum_shear
+        self.bear = max_bearing_stress
 
     def get_stress(self):
         return self.cr
 
     def get_E(self):
         return self.E
+
+    def get_G(self):
+        return self.g
+
+    def get_shear(self):
+        return self.sh
+
+    def get_bear(self):
+        return self.bear
