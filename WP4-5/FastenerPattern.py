613d05008c79f0a670b350b299f11595eddc2932
DanielsGor <d.gorovojs@student.tudelft.nl>
2021-11-15 16:06:19 +0100

    Diameter

--- WP4-5/FastenerPattern.py
+++ WP4-5/FastenerPattern.py
@@ -1,28 +1,19 @@
-import math 
+
+from Classes import *
+from Iterations_Lug import *
 
 F_y = 2118.2364
 F_z = 4069.2436105263155
 F_x = 353.0394
 M_y = 141.21576000000002
 
+F = Loads (353.0394,2118.2364,4069.2436105263155,0,141.21576000000002,0)
+
 n = 4 #number of bolts
 
-E = 200 * 10**9 #Youngs modulus for steel
+w = 200.0
+h = 150.0
 
-M_x_plate = F_y * 0.02
-
-w = 200
-h = 150 
-
-F_z_max = F_z + M_y/w*2 + M_x_plate/h*2
-
-
-sigma_yield = 250 * 10**6 #Yield strength steel
-
-A_tot = F_z / sigma_yield
-
-A = A_tot / n
-
-D = math.sqrt(4*A/math.pi)
+D = Min_Fastener_Diameter_Tension(F,steel, n, w, h, 0.02)
 
 print("Bolt D    ", D)
