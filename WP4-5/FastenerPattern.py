1f04ebf97d184305904e810a7c58135540c5051e
DanielsGor <d.gorovojs@student.tudelft.nl>
2021-11-22 15:04:29 +0100

    Gets a non-feasable but working design for back plate & fasteners

--- WP4-5/FastenerPattern.py
+++ WP4-5/FastenerPattern.py
@@ -9,7 +9,7 @@ F_z = 4069.2436105263155
 F_x = 353.0394
 M_y = 141.21576000000002
 gap = 0.02
-thick = 0.01
+thickness = 0.01
 
 aluminium = Material(
     name='aluminium',
@@ -44,14 +44,19 @@ F = Loads(353.0394,2118.2364,4069.2436105263155,0,141.21576000000002,0)
 
 n = 4 #number of bolts
 
-w = 0.200
-h = 0.150
+#w = 0.200
+#h = w
 
-def GetSFs (F,thick,w,h,n):
+def inch_to_m(l):
+    return l*2.54/100
 
-    D_1st = 1.5 * Min_Fastener_Diameter_Tension(F,steel, n, w, h, gap)
+def GetSFs (F,D_1st,thickness,w,h,n):
 
-    plate = Plate(n,D_1st, thick, w+D_1st*1.5, h+D_1st*1.5)
+    D_not_fail = Min_Fastener_Diameter_Tension(F,steel, n, w, h, gap)
+
+    SF_Tension_Failure = D_1st/D_not_fail
+
+    plate = Plate(n,D_1st, thickness, w+D_1st*1.5, h+D_1st*1.5)
 
     plate.get_mass(aluminium)
 
@@ -67,29 +72,53 @@ def GetSFs (F,thick,w,h,n):
 
     plate_fm = plate.force_moment(plate_ftdm,F.F_x, F.F_y)
 
-    shearStress = plate.fastener_shear_stress(D_1st, thick, plate_fm)
-
-#print("Shear stress in fasteners   ", shearStress/(10**6), "MPa")
+    shearStress = plate.fastener_shear_stress(D_1st, thickness, plate_fm)
 
     shearYieldStrength = 0.58 * steel.y
 
     SF_Shear_Failure = shearYieldStrength / shearStress
 
-#print("Shear stress Safety Factor  ", SF_Shear_Failure)
-
-#print("Bolt D    ", D_1st)
-
-
-    bearing_Stress = plate.pull_through_fail(n,D_1st,D_1st*3,np.transpose(cord),thick,thick,36.0,gap)
+    bearing_Stress = plate.pull_through_fail(n,D_1st,D_1st*3,np.transpose(cord),thickness,thickness,36.0,gap)
 
     bearing_Stress_Max = max(bearing_Stress)
 
     SF_bearing_failure = aluminium.y/bearing_Stress_Max
 
-    return SF_Shear_Failure, SF_bearing_failure
+    return SF_Shear_Failure, SF_bearing_failure, SF_Tension_Failure
 
-SFs = GetSFs(F, thick, w, h, n)
+thickness = inch_to_m(7/8)
+D = inch_to_m(5/8)
+w = inch_to_m(3+13/16)
+W = inch_to_m(5)
+h=w
+
+#ratios
+w_over_t = w/thickness
+D_over_t = D/thickness
+h = w
+W_over_w = W/w
+
+SFs = GetSFs(F,D, thickness, w, h, n)
+
+while (min(SFs)>1.5):
+    thickness = thickness - 0.00025
+    w = w_over_t * thickness
+    h = w
+    D = D_over_t * thickness
+    SFs = GetSFs(F,D, thickness, w, h, n)
+    
+
+thickness = thickness + 0.00025
+w = w_over_t * thickness
+h = w
+W = W_over_w * w
+D = D_over_t * thickness
+SFs = GetSFs(F,D, thickness, w, h, n)
+
+print("Safety Factors: Shear, Pull-through, Tension")
 print(SFs)
-#print("Stress Bearing  ",bearing_Stress)
-#print("Stress Bearing MAX ",bearing_Stress_Max)
-#print("Bearing Safety Factor  ", aluminium.y/bearing_Stress_Max)
+print("Thickness (mm)   ", thickness*1000)
+print("Width = Height (mm)  ",W*1000)
+print("Distance between fasteners (mm)  ",w*1000)
+print("Fastener diameter (mm)  ",D*1000)
+
