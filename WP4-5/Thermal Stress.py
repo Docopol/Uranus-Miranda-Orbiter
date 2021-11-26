f8ee49f49fac9cc06716c3ac9669a0a17ae876c0
JanOppelaar <90770036+JanOppelaar@users.noreply.github.com>
2021-11-26 16:38:51 +0100

    thrml

--- WP4-5/Thermal Stress.py
+++ WP4-5/Thermal Stress.py
@@ -5,14 +5,22 @@ T_ref = 288
 T_max = 298.5
 T_min = 279
 
+A_sm = ...
+Phi = ...
+
 DT_max = T_max - T_ref
 DT_min = T_min - T_ref
 
-fas_mat = material_dict['Al2014T6'] # fastener material
-plt_mat = material_dict['Al2014T6'] # material of clamped parts
+fas_mat = material_dict['Al2014T6']  # fastener material
+plt_mat = material_dict['Al7075T6']  # material of clamped parts
 
 alpha_b = fas_mat.get_TEC()
 alpha_c = plt_mat.get_TEC()
 
+
 def maxT_stress(DT, E, A_sm, alpha_b, alpha_c, Phi):
-    return (alpha_c-alpha_b)*DT*E*A_sm*(1-Phi)
+    return (alpha_c - alpha_b) * DT * E * A_sm * (1 - Phi)
+
+
+print("thermal stress at maximum temp:", maxT_stress(DT_max, fas_mat.get_E(), A_sm, alpha_b, alpha_c, Phi))
+print("thermal stress at minimum temp:", maxT_stress(DT_min, fas_mat.get_E(), A_sm, alpha_b, alpha_c, Phi))
