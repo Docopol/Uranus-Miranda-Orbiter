5ab3483031896641b0f587c0ba2f434afa464e3d
JanOppelaar <90770036+JanOppelaar@users.noreply.github.com>
2021-11-26 14:36:37 +0100

    small change

--- WP4-5/Thermal Stress.py
+++ WP4-5/Thermal Stress.py
@@ -5,8 +5,6 @@ T_ref = 288
 T_max = 298.5
 T_min = 279
 
-L = ... # thickness of plate and wall (t2 + t3 in fig. 4.1 in reader)
-
 DT_max = T_max - T_ref
 DT_min = T_min - T_ref
 
@@ -16,5 +14,5 @@ plt_mat = material_dict['Al2014T6'] # material of clamped parts
 alpha_b = fas_mat.get_TEC()
 alpha_c = plt_mat.get_TEC()
 
-def maxT_stress(DT, E, A, alpha_b, alpha_c, Phi):
-    return (alpha_c-alpha_b)*DT*E*A*(1-Phi)
+def maxT_stress(DT, E, A_sm, alpha_b, alpha_c, Phi):
+    return (alpha_c-alpha_b)*DT*E*A_sm*(1-Phi)
