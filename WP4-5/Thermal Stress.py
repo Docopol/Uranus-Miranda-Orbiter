1fd58763a8d666254c8b5ceda1376b021d8d00e5
JanOppelaar <90770036+JanOppelaar@users.noreply.github.com>
2021-11-27 14:40:29 +0100

    thermal stuff

--- WP4-5/Thermal Stress.py
+++ WP4-5/Thermal Stress.py
@@ -1,18 +1,26 @@
 # all temperatures will be in kelvin
 from Constants import *
+import numpy as np
 
 T_ref = 288
-T_max = 298.5
+T_max = 298.5  # IS NOT ACTUAL TMAX probably higher
 T_min = 279
 
-A_sm = ... # probably the area calculated with the diameter of the bolt
-Phi = ... # no f clue lmao, wtf are they talking about, phi is the force ratio, but what the hell does that mean
+D_fo = ...  # i think these can be found in classes.py... standby,
+D_fi = ...
+t = ...
+
+fas_mat = StA992  # fastener material
+plt_mat = Al7075T6  # material of clamped parts
+
+delta_a = (4*t)/(plt_mat.get_E()*np.pi*(D_fo**2 - D_fi**2))  # need to define t, D_fo and D_fi
+delta_b = (1/fas_mat.get_E())  # not finished
+Phi = delta_a/(delta_a + delta_b)  # force ratio
+
+A_sm = np.pi*(D_fi/2)**2
 
 DT_max = T_max - T_ref
 DT_min = T_min - T_ref
-
-fas_mat = material_dict['Al2014T6']  # fastener material
-plt_mat = material_dict['Al7075T6']  # material of clamped parts
 
 alpha_b = fas_mat.get_TEC()
 alpha_c = plt_mat.get_TEC()
