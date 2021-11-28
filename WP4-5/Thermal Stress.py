cb1a13a241460437cd8a6c24f9bae66171f83e1f
JanOppelaar <90770036+JanOppelaar@users.noreply.github.com>
2021-11-28 20:59:58 +0100

    thermal stuff

--- WP4-5/Thermal Stress.py
+++ WP4-5/Thermal Stress.py
@@ -6,8 +6,9 @@ T_ref = 288
 T_max = 298.5  # IS NOT ACTUAL TMAX probably higher
 T_min = 279
 
-D_fo = ...  # outer diameter of the bolt
-D_fi = ...  # inner diameter of the bolt
+D_fi = ...  # hole size
+D_fo = ...  # Bolt diameter
+D_Nut = ...  # Nut diameter
 t_w = ...  # thickness of the wall
 t_p = ...  # thickness of the plate
 
@@ -15,7 +16,7 @@ fas_mat = StA992  # fastener material
 plt_mat = Al7075T6  # material of plate
 wll_mat = Al7075T6  # material of wall
 
-plt_mat_E = (t_w*wll_mat.get_E()+t_p*plt_mat.get_E())/(t_w+t_p)
+plt_mat_E = (t_w * wll_mat.get_E() + t_p * plt_mat.get_E()) / (t_w + t_p)
 
 DT_max = T_max - T_ref
 DT_min = T_min - T_ref
@@ -24,13 +25,17 @@ alpha_a = wll_mat.get_TEC()
 alpha_b = fas_mat.get_TEC()
 alpha_c = plt_mat.get_TEC()
 
-alpha_c = (t_w*alpha_a+t_p*alpha_c)/(t_w+t_p)
+alpha_c = (t_w * alpha_a + t_p * alpha_c) / (t_w + t_p)
 
-delta_a = (4*(t_w+t_p))/(plt_mat_E*np.pi*(D_fo**2 - D_fi**2))
-delta_b = ...  # (1/fas_mat.get_E())*sum(L_i/A_i)+(L_nsub)/(E_n*A_nom)  # not finished
-Phi = delta_a/(delta_a + delta_b)  # force ratio
+L_over_A = [(0.5 * D_fo) / (np.pi * (D_fo / 2) ** 2), (t_p + t_w) / (np.pi * (D_fi / 2) ** 2),
+            (0.4 * D_fo) / (np.pi * (D_Nut / 2) ** 2)]
 
-A_sm = np.pi*(D_fi/2)**2
+delta_a = (4 * (t_w + t_p)) / (plt_mat_E * np.pi * (D_fo ** 2 - D_fi ** 2))
+delta_b = (fas_mat.get_E() ** -1) * sum(L_over_A)
+
+Phi = delta_a / (delta_a + delta_b)  # force ratio
+
+A_sm = np.pi * (D_fi / 2) ** 2
 
 
 def maxT_stress(DT, E, A_sm, alpha_b, alpha_c, Phi):
