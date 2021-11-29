fe252d99f099f80aac574bd5ee0e4214edc18db1
docopol <35876855+Docopol@users.noreply.github.com>
2021-11-29 17:17:42 +0100

    Added Inner pressure and Euler Columns BucklingF

--- WP5/tank.py
+++ WP5/tank.py
@@ -1,10 +1,28 @@
-import calc, constants
+import materials, standards
+import numpy as np
 
-class tank(self, diameter, length, thickness, material):
-	self.d = diameter
-	self.l = length
-	self.t = thickness
-	self.mat = material
+class Tank(self, radius, length, thickness1, thickness2, material):
+	def __init__(self):
+		self.r = radius
+		self.l = length
+		self.t1 = thickness1
+		self.t2 = thickness2
+		self.mat = material
 
-	def InnerPressureFailure(self):
-		radialBurstPressure = self.material["yield_stress"]
+	def InnerPressureF(self, radius, length, thickness1, thickness2, material):
+		radialBurstPressureCyl = material["t_yield_stress"]*thickness1/radius
+		radialBurstPressureEnd = material["t_yield_stress"]*thickness2/radius
+
+		failurePressure = np.minimum(radialBurstPressureCyl, radialBurstPressureCyl)
+
+		return failurePressure
+
+	def EulerColumnBucklingF(self, radius, length, thickness1, thickness2, material):
+		ICylinder = np.pi/4*(radius**4-(radius-thickness1)**4)
+		areaCylinder = np.pi*radius**2
+		criticalStressE = ((np.pi**2)*material["E_modulus"]*ICylinder)/(areaCylinder*length**2)
+
+		return criticalStressE
+
+	def ShellBuckling(self):
+		continue
