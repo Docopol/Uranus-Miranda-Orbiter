d7fa8c96ed1f925e1c1f2a476ce03219df53074e
JanOppelaar <90770036+JanOppelaar@users.noreply.github.com>
2021-12-01 14:54:11 +0100

    Changes by kamal

--- WP5/Dynamic_loads.py
+++ WP5/Dynamic_loads.py
@@ -4,14 +4,11 @@ from Constants import *
 
 def frequency_SC(E, M, A, I, L):
     k_y = E * A / L  # Longitudinal direction
-    k_xz = 3 * E * I / np.power(L, 3)  # Lateral direction
 
-    f_n1 = np.sqrt(k_y / M) / (2 * np.pi)
-    f_n2 = np.sqrt(k_xz / M) / (2 * np.pi)
+    f_nat = np.sqrt(k_y / M) / (2 * np.pi)
+    f_launcher = 30
 
-    f_nat = np.minimum(f_n1, f_n2)
-
-    return f_nat, f_n1, f_n2
+    return f_nat, f_nat > f_launcher
 
 
 print(frequency_SC(73.1e9, 18119.35, 2 * np.pi * 1.8 * 0.0005, np.pi * np.power(1.8, 3) * 0.0005, 8.235))
