eaf531563f55d77ada0da9d815a6fe109cb8edfa
JanOppelaar <90770036+JanOppelaar@users.noreply.github.com>
2021-12-01 14:00:10 +0100

    things idk

--- WP5/Dynamic_loads.py
+++ WP5/Dynamic_loads.py
@@ -3,8 +3,8 @@ from Constants import *
 
 
 def frequency_SC(M, E, A, I, L):
-    k_y = E * A / L                     # Longitudinal direction
-    k_xz = 3 * E * I / np.power(L, 3)      # Lateral direction
+    k_y = E * A / L  # Longitudinal direction
+    k_xz = 3 * E * I / np.power(L, 3)  # Lateral direction
 
     f_n1 = np.sqrt(k_y / M) / (2 * np.pi)
     f_n2 = np.sqrt(k_xz / M) / (2 * np.pi)
@@ -13,4 +13,5 @@ def frequency_SC(M, E, A, I, L):
 
     return f_nat, f_n1, f_n2
 
-print(frequency_SC(18119.35, 73.1e9, 2*np.pi*1.8*0.0005, np.pi*np.power(1.8,3)*0.0005, 8.235))
+
+print(frequency_SC(18119.35, 73.1e9, 2 * np.pi * 1.8 * 0.0005, np.pi * np.power(1.8, 3) * 0.0005, 8.235))
