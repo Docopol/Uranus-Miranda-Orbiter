060ee5cc1f0f3c228c37916d81c50c6e05abdf4c
swelters <s.j.welters@student.tudelft.nl>
2021-12-15 16:51:54 +0100

    Material change

--- WP5/materials.py
+++ WP5/materials.py
@@ -16,14 +16,32 @@ Al6061T6 = {        # done
     "TEC": 24e-6
 }
 
-MgAM60 = {
-    "name": 'Mg-Am60',
-    "E_modulus": 45e9,
-    "t_yield_stress": 130e6,
-    "poisson_ratio": 0.35,
-    "density": 1800,
-    "TEC": 26e-6
+Al2219T6 = {        # done
+    "name": 'Al2219-T6',
+    "E_modulus": 72e9,
+    "t_yield_stress": 280e6,
+    "poisson_ratio": 0.33,
+    "density": 2840,
+    "TEC": 22e-6
 }
+
+Al7075T6 = {        # done
+    "name": 'Al7075-T6',
+    "E_modulus": 71.7e9,
+    "t_yield_stress": 503e6,
+    "poisson_ratio": 0.33,
+    "density": 2810,
+    "TEC": 23.6e-6
+}
+
+# MgAM60 = {
+#     "name": 'Mg-Am60',
+#     "E_modulus": 45e9,
+#     "t_yield_stress": 130e6,
+#     "poisson_ratio": 0.35,
+#     "density": 1800,
+#     "TEC": 26e-6
+# }
 
 StA992 = {
     "name": 'St-A992',
@@ -34,23 +52,25 @@ StA992 = {
     "TEC": 12e-6
 }
 
-Al7075T6 = {
-    "name": 'Al7075-T6',
-    "E_modulus": 71.7e9,
-    "t_yield_stress": 503e6,
-    "poisson_ratio": 0.33,
-    "density": 2810,
-    "TEC": 23.6e-6
+SS301 = {
+    "name": 'SS-301',
+    "E_modulus": 200e9,
+    "t_yield_stress": 1080e6,
+    "poisson_ratio": 0.28,
+    "density": 7800,
+    "TEC": 17e-6
 }
 
-Al2024T3 = {
-    "name": 'Al2024-T3',
-    "E_modulus": 73.1 * 10 ** 9,
-    "t_yield_stress": 345 * 10 ** 6,
-    "poisson_ratio": 0.33,
-    "density": 2780,
-    "TEC": 23.2 * 10 ** -6
-}
+
+
+# Al2024T3 = {
+#     "name": 'Al2024-T3',
+#     "E_modulus": 73.1 * 10 ** 9,
+#     "t_yield_stress": 345 * 10 ** 6,
+#     "poisson_ratio": 0.33,
+#     "density": 2780,
+#     "TEC": 23.2 * 10 ** -6
+# }
 
 # Al2024T4 = { # done
 #     "name":'Al2024-T4',
@@ -59,23 +79,23 @@ Al2024T3 = {
 #     "density":2780,
 #     "TEC":23.2 * 10 ** -6
 # }
-
-St8630 = {
-    "name": 'St8630',
-    "E_modulus": 187 * 10 ** 9,
-    "t_yield_stress": 550 * 10 ** 6,
-    "poisson_ratio": 0.28,
-    "density": 7850,
-    "TEC": 11.2 * 10 ** -6
-}
-St4130 = {
-    "name": 'St4130',
-    "E_modulus": 190 * 10 ** 9,
-    "t_yield_stress": 460 * 10 ** 6,
-    "poisson_ratio": 0.28,
-    "density": 7850,
-    "TEC": 25.2 * 10 ** -6
-}
+#
+# St8630 = {
+#     "name": 'St8630',
+#     "E_modulus": 187 * 10 ** 9,
+#     "t_yield_stress": 550 * 10 ** 6,
+#     "poisson_ratio": 0.28,
+#     "density": 7850,
+#     "TEC": 11.2 * 10 ** -6
+# }
+# St4130 = {
+#     "name": 'St4130',
+#     "E_modulus": 190 * 10 ** 9,
+#     "t_yield_stress": 460 * 10 ** 6,
+#     "poisson_ratio": 0.28,
+#     "density": 7850,
+#     "TEC": 25.2 * 10 ** -6
+# }
 # http://www.matweb.com/search/DataSheet.aspx?MatGUID=e1ccebe90cf94502b35c2a4745f63593
 
 Ti6Al4V = {
@@ -88,14 +108,14 @@ Ti6Al4V = {
 }
 #http://asm.matweb.com/search/SpecificMaterial.asp?bassnum=MTP641
 
-MgAZ91CT6 = {
-    "name": 'MgAZ91C-T6',
-    "E_modulus": 44.8 * 10 ** 9,
-    "t_yield_stress": 145 * 10 ** 6,
-    "poisson_ratio": 0.35,
-    "density": 1810,
-    "TEC": 26 * 10 ** -6
-}
+# MgAZ91CT6 = {
+#     "name": 'MgAZ91C-T6',
+#     "E_modulus": 44.8 * 10 ** 9,
+#     "t_yield_stress": 145 * 10 ** 6,
+#     "poisson_ratio": 0.35,
+#     "density": 1810,
+#     "TEC": 26 * 10 ** -6
+# }
 # http://www.matweb.com/search/DataSheet.aspx?MatGUID=8c8cad8fe20544508f41b4a1300af4a1
 
 material_dict = {'Al2014T6': Al2014T6, 'Al7075T6': Al7075T6, 'Al6061T6': Al6061T6, 'MgAM60': MgAM60,
